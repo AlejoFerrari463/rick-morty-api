c1c284305919bcbfdffa5c369cbc146478861506
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-07-22 10:26:55 -0300

    carga inicial

---
+++ javascript/index.js
@@ -0,0 +1,36 @@
+
+
+const url = "https://rickandmortyapi.com/api/character?page=1"
+
+function cargarUrl(url){
+
+    fetch(url)
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+
+       
+
+
+        data.results.forEach(element => {
+
+           
+
+            const {name,episode} = element
+            
+            console.log(name,"Episodios:",episode.length)
+
+
+        })
+
+
+    })
+    .catch((error)=>{
+        console.log("error",error)
+    })
+
+} 
+
+
+cargarUrl(url)
