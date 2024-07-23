6e2ed14bb941fd0952bf8904dc519bba77c5f2df
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-07-23 00:26:30 -0300

    agregado de cards, nombre, especie e imagen

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,8 @@
 
 
 const url = "https://rickandmortyapi.com/api/character?page=1"
+const contenedorCards = document.querySelector(".cards-contenedor")
+
 
 function cargarUrl(url){
 
@@ -10,16 +12,31 @@ function cargarUrl(url){
     })
     .then((data)=>{
 
+      
 
         data.results.forEach(element => {
 
-           
-            const {name,episode} = element
+    
+        
+            const {name,episode,species,image} = element
             
-            console.log(name,"Episodios:",episode.length)
+            console.log(element)
+           
+            const card = document.createElement("div")
+            card.classList.add("cards")
+            card.innerHTML=`
+                ${name}
+                <div>${species}</div>
+                <img src="${image}">
+            `
+            contenedorCards.appendChild(card)
 
 
         })
+        // console.log("")
+        // if(data.info.next){
+        //     cargarUrl(data.info.next)
+        // }
 
 
     })
